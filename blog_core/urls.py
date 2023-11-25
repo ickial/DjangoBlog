1c1c124f30791db1bab61c5f8dc4641d508df067
ickial <konetskiya@gmail.com>
2023-11-25 13:15:14 +0100

    add same views

--- blog_core/urls.py
+++ blog_core/urls.py
@@ -1,26 +1,11 @@
-# from django.conf.urls import url
-# from . import views
-#
-# urlpatterns = [
-#     # post views
-#     url(r'^$', views.post_list, name='post_list'),
-#     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
-#         r'(?P<post>[-\w]+)/$',
-#         views.post_detail,
-#         name='post_detail'),
-# ]
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from django.urls import path
 from . import views
 
 app_name = "blog-core"
 
 urlpatterns = [
     path("posts/", views.post_list, name="post_list"),
-    path("post_created_data/<int:POST>/<int:POST>>", views.created_data_posts, name='created_data_posts'),
     path("post_detail/<int:post>", views.post_detail, name="post_detail"),
-    path("posts_by_author/<str:username_str>", views.post_by_author, name='post_by_author')
+    path("posts/by_author/<str:username_str>", views.posts_by_author, name='posts_by_author'),
+    path("posts/<int:year_str>/<int:mouth_str>", views.posts_by_date, name='publish_date_posts')
 ]
-
-# 8000/posts_by_author/author1
-# http://127.0.0.1:8000/posts/2003/08
